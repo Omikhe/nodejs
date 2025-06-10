8573d28c324720370b42e6b3c1aa3819a4be42ef
Omikhe <msakamike84@gmail.com>
2025-06-10 17:52:39 +0200

    make a bunch of changes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var http = require('http');
 var dt = require('./module');
+var url = require('url')
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
+  var q = url.parse(req.url, true).query;
+  //   res.write("The date and time are currently: " + dt.myDateTime());
+//   res.write(req.url)
+var txt = q.month + " " + q.year;
+  res.end(txt);
 }).listen(8080); 
