4069a88edcf1649d310da11b3c1eae697ec270a2
Omikhe <msakamike84@gmail.com>
2025-06-11 06:55:53 +0200

    implement upload logic

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var http = require('http');
-var url = require('url');
+var formidable = require('formidable');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.filepath;
+      var newpath = 'C:/Users/Omikhe/' + files.filetoupload.originalFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
     return res.end();
-  });
-}).listen(8080);
+  }
+}).listen(8080); 
